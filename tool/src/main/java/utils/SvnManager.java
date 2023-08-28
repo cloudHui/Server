0c9894f128273e420e97cdc04e2ea17efcb472c9
liuyunhui <liuyunhui@coolfishgames.com>
2023-08-28 07:53:36 +0000

    update
    svn 版本检测和更新

---
+++ tool/src/main/java/utils/SvnManager.java
@@ -0,0 +1,69 @@
+package utils;
+
+import java.util.List;
+
+import utils.utils.ExecCommand;
+import utils.utils.StringUtils;
+
+public class SvnManager {
+
+	private static SvnManager instance;
+
+	public static SvnManager getInstance() {
+		return instance;
+	}
+
+	private String REV = "Revision: ";
+
+	public SvnManager() {
+		String path = System.getProperty("user.dir");
+		List<String> exeCommands = ExecCommand.exeCommand("svn info " + path);
+		jarUrl = ExecCommand.getExeCommandResult(exeCommands, "Url: ");
+		jarLastVersion = ExecCommand.getExeCommandResult(exeCommands, REV);
+	}
+
+	/**
+	 * 运行包地址
+	 */
+	private String jarUrl;
+
+	/**
+	 * 上一次检测版本
+	 */
+	private String jarLastVersion;
+
+	public String getJarUrl() {
+		return jarUrl;
+	}
+
+	public String getJarLastVersion() {
+		return jarLastVersion;
+	}
+
+	/**
+	 * 获取当前版本
+	 */
+	public String getVersion() {
+		return ExecCommand.exeCommand(jarUrl, REV);
+	}
+
+	/**
+	 * 版本是否更新
+	 */
+	public boolean checkJarVersionUpdate() {
+		String jarLastVersion = getJarLastVersion();
+		if (jarLastVersion == null || jarLastVersion.length() <= 0) {
+			return false;
+		}
+		String currVersion = getVersion();
+
+		return StringUtils.versionBigger(jarLastVersion, currVersion);
+	}
+
+	/**
+	 * 更新代码
+	 */
+	public void jarUpdate() {
+		ExecCommand.exeCommand("svn update " + getJarUrl());
+	}
+}
