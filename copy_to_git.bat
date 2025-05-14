e95775adcd071999ada325b042a12c063075085d
cloudHui <1056405256@qq.com>
2025-05-14 12:01:00 +0800

    update
    添加自动提交更新修改删除到远端

--- copy_to_git.bat
+++ copy_to_git.bat
@@ -83,5 +83,36 @@ Xcopy %if2_java_path%\room\Room.jar %to_path%\Room /y
 
 Xcopy %if2_bat_path%\room\bat\ %to_path%\room /y
 
-echo 成功处理所有配置文件
+echo 成功复制所有配置文件
+
+:: 切换到Git仓库目录
+cd /d %github%
+
+:: 添加所有新文件（未跟踪的文件）
+for /f %%i in ('git ls-files --others --exclude-standard') do (
+    git add %%i
+)
+
+:: 移除所有已从工作目录中删除但未从Git仓库中删除的文件
+for /f %%i in ('git ls-files --deleted') do (
+    git rm %%i
+)
+
+:: 添加所有修改过的文件
+git add -u
+
+:: 定义变量
+set COMMIT_MSG="Automated commit: Modified, Deleted, and Added files"
+:: 提交所有更改
+for /f %%i in ('git status --porcelain') do (
+    git commit -m %COMMIT_MSG%
+    git push origin main
+    echo 提交成功：所有修改、删除和新增的文件都已提交。
+    goto end
+)
+echo 没有需要提交的更改。
+
+:end
+
+echo 成功提交所有文件
 pause
