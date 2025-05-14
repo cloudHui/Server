27da75ea9e54ee0da8c14eda8f91b5f3be7ecc95
cloudHui <1056405256@qq.com>
2025-05-14 14:30:47 +0800

    update
    修复休眠脚本命令

--- gate/bat/UpdateGit.bat
+++ gate/bat/UpdateGit.bat
@@ -3,6 +3,6 @@ set cur_dir=%cd%
 if exist %cur_dir%\gitUpdate.log (echo update: %date% %time%  >> %cur_dir%\gitUpdate.log) else echo create: %date% %time% > %cur_dir%\gitUpdate.log
 git pull
 git log -1 --pretty=format:%%H >>  %cur_dir%\gitUpdate.log
-sleep 1
+timeout /t 1
 call start.bat
 exit
--- update.bat
+++ update.bat
@@ -1,11 +1,16 @@
 @echo off
 chcp 65001
 echo "先 git update 更新最新代码"
-sleep 1
+timeout /t 1
+
 git update
 
 echo "再打包代码"
-sleep 1
+timeout /t 1
+::for /L %%i in (1,1,10) do (
+  ::  echo ". "
+   :: timeout /t 1
+::)
 mvn install
 echo "远端代码更新成功 本地代码打包成功"
 pause
